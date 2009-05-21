4cd4da9cb2c06ed4ff5a8ebfd89edca5ef36d212
Yossef Mendelssohn <ymendel@pobox.com>
2009-05-20 21:20:22 -0500

    Creating new-article view.
    References http://www.pivotaltracker.com/story/show/713066
    Closes http://www.pivotaltracker.com/story/show/713066

---
+++ spec/views/admin/articles/new.html.haml_spec.rb
@@ -0,0 +1,44 @@
+require File.expand_path(File.join(File.dirname(__FILE__), %w[.. .. .. spec_helper]))
+
+describe 'admin/articles/new.html.haml' do
+  before :each do
+    assigns[:article] = @article = Article.new
+  end
+  
+  def do_render
+    render 'admin/articles/new.html.haml'
+  end
+  
+  it 'should have a new-article form' do
+    do_render
+    response.should have_tag('form[id=?]', 'new_article')
+  end
+  
+  describe 'new-article form' do
+    it 'should use the article create action' do
+      do_render
+      response.should have_tag('form[id=?][action=?][method=?]', 'new_article', admin_articles_path, 'post')
+    end
+    
+    it 'should have a title input' do
+      do_render
+      response.should have_tag('form[id=?]', 'new_article') do
+        with_tag('input[type=?][name=?]', 'text', 'article[title]')
+      end
+    end
+    
+    it 'should have a content input' do
+      do_render
+      response.should have_tag('form[id=?]', 'new_article') do
+        with_tag('textarea[name=?]', 'article[content]')
+      end
+    end
+    
+    it 'should allow for submitting the form' do
+      do_render
+      response.should have_tag('form[id=?]', 'new_article') do
+        with_tag('input[type=?]', 'submit')
+      end
+    end
+  end
+end
