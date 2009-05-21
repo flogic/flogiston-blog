94a485ad0e0977c207668a715819ed670065c048
Yossef Mendelssohn <ymendel@pobox.com>
2009-05-21 16:09:24 -0500

    Adding edit-article form.
    Same as new-article form, so refactored that out to a partial.
    
    References http://www.pivotaltracker.com/story/show/713077

---
+++ spec/views/admin/articles/edit.html.haml_spec.rb
@@ -0,0 +1,60 @@
+require File.expand_path(File.join(File.dirname(__FILE__), %w[.. .. .. spec_helper]))
+
+describe 'admin/articles/edit.html.haml' do
+  before :each do
+    assigns[:article] = @article = Article.generate!(:title => 'Who Is To Say?', :content => 'Nobody, really.')
+  end
+  
+  def do_render
+    render 'admin/articles/edit.html.haml'
+  end
+  
+  it 'should have an edit-article form' do
+    do_render
+    response.should have_tag('form[id=?]', "edit_article_#{@article.id}")
+  end
+  
+  describe 'edit-article form' do
+    it 'should use the article update action' do
+      do_render
+      response.should have_tag('form[id=?][action=?][method=?]', "edit_article_#{@article.id}", admin_article_path(@article), 'post') do
+        with_tag('input[name=?][value=?]', '_method', 'put')
+      end
+    end
+    
+    it 'should have a title input' do
+      do_render
+      response.should have_tag('form[id=?]', "edit_article_#{@article.id}") do
+        with_tag('input[type=?][name=?]', 'text', 'article[title]')
+      end
+    end
+    
+    it 'should populate the title input' do
+      do_render
+      response.should have_tag('form[id=?]', "edit_article_#{@article.id}") do
+        with_tag('input[name=?][value=?]', 'article[title]', @article.title)
+      end
+    end
+    
+    it 'should have a content input' do
+      do_render
+      response.should have_tag('form[id=?]', "edit_article_#{@article.id}") do
+        with_tag('textarea[name=?]', 'article[content]')
+      end
+    end
+    
+    it 'should populate the content input' do
+      do_render
+      response.should have_tag('form[id=?]', "edit_article_#{@article.id}") do
+        with_tag('textarea[name=?]', 'article[content]', :text => @article.content)
+      end
+    end
+    
+    it 'should allow for submitting the form' do
+      do_render
+      response.should have_tag('form[id=?]', "edit_article_#{@article.id}") do
+        with_tag('input[type=?]', 'submit')
+      end
+    end
+  end
+end
