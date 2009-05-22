de2548dc5f9012fdb6fb451c966acea9f87a056b
Yossef Mendelssohn <ymendel@pobox.com>
2009-05-21 22:19:03 -0500

    Adding publication time to Article.
    Publication time can be set explicitly, but it defaults to creation time.
    This will clean up some tests and behavior instead of overloading the concept of
    creation time to also mean publication.
    
    References http://www.pivotaltracker.com/story/show/721644
    Closes http://www.pivotaltracker.com/story/show/721644

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ActiveRecord::Base
+  
+  private
+  
+  def default_publication_time
+    update_attributes(:published_at => created_at) unless published_at
+  end
+  after_save :default_publication_time
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -13,5 +13,40 @@ describe Article do
     it 'should have a title' do
       @article.should respond_to(:title)
     end
+    
+    it 'should have a creation time' do
+      @article.should respond_to(:created_at)
+    end
+    
+    it 'should have a publication time' do
+      @article.should respond_to(:published_at)
+    end
+  end
+  
+  describe 'publication time' do
+    it 'should be settable' do
+      t = Time.zone.local(2007, 3, 4, 1, 2, 3)
+      article = Article.generate!(:published_at => t)
+      article.reload
+      article.published_at.should == t
+    end
+    
+    it 'should allow a future time' do
+      t = Time.zone.now + 12345
+      t = Time.zone.local(t.year, t.month, t.day, t.hour, t.min, t.sec)  # milliseconds, yay
+      article = Article.generate!(:published_at => t)
+      article.reload
+      article.published_at.should == t
+    end
+    
+    it 'should default to creation time' do
+      article = Article.generate!
+      article.published_at.should == article.created_at
+    end
+    
+    it 'should be set to creation time if explicitly set to nil' do
+      article = Article.generate!(:published_at => nil)
+      article.published_at.should == article.created_at
+    end
   end
 end
