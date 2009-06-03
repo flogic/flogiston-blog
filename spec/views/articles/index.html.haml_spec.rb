70f67eea78499666636ffda4a10e73a4f9ad9e3f
Kevin R. Barnes <vinbarnes@gmail.com>
2009-06-02 20:45:22 -0500

    Update index view to show list of articles.

--- spec/views/articles/index.html.haml_spec.rb
+++ spec/views/articles/index.html.haml_spec.rb
@@ -2,33 +2,25 @@ require File.expand_path(File.join(File.dirname(__FILE__), %w[.. .. spec_helper]
 
 describe 'articles/index.html.haml' do
   before :each do
-    assigns[:article] = @article = Article.generate!(:title => 'A Day in the Life', :content => 'Just a bunch of boring, really.')
+    assigns[:articles] = @articles = Array.new(10) { Article.generate!(:title => 'A Day in the Life', :content => 'Just a bunch of boring, really.') }
+    @title   = @articles[0].title
+    @content = @articles[0].content
   end
   
   def do_render
     render 'articles/index.html.haml'
   end
   
-  it 'should include the article title' do
+  it 'should include the article titles' do
     do_render
-    response.should have_text(Regexp.new(Regexp.escape(@article.title)))
+    response.should have_text(Regexp.new(Regexp.escape(@title)))
   end
   
-  it 'should include the article content' do
+  it 'should include links to the articles' do
     do_render
-    response.should have_text(Regexp.new(Regexp.escape(@article.content)))
+    response.should have_tag('a', 10)
   end
-  
-  it 'should format the article content as markdown' do
-    @article.content = "
-  * one
-  * two
-  * three
-"
-    do_render
-    response.should have_tag('li', /one/)
-  end
-  
+    
   it 'should not error if the article is nil' do
     assigns[:article] = nil
     do_render
