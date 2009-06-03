bf3e674d925b3dc27d66bfe0c005d21359acc4d0
Kevin R. Barnes <vinbarnes@gmail.com>
2009-06-03 16:51:17 -0500

    Forgot to add the pagination links to articles index.
    Added pagination and cleaned up specs with yossef's help.

--- spec/views/articles/index.html.haml_spec.rb
+++ spec/views/articles/index.html.haml_spec.rb
@@ -1,30 +1,66 @@
 require File.expand_path(File.join(File.dirname(__FILE__), %w[.. .. spec_helper]))
 
 describe 'articles/index.html.haml' do
-  before :each do
-    assigns[:articles] = @articles = Array.new(10) { Article.generate!(:title => 'A Day in the Life', :content => 'Just a bunch of boring, really.') }
-    @title   = @articles[0].title
-    @content = @articles[0].content
-  end
-  
   def do_render
     render 'articles/index.html.haml'
   end
   
-  it 'should include the article titles' do
-    do_render
-    response.should have_text(Regexp.new(Regexp.escape(@title)))
-  end
-  
-  it 'should include links to the articles' do
-    do_render
-    response.should have_tag('ul.articles') do
-      with_tag('a[href*=?]', Regexp.new(Regexp.escape(articles_path + "/") + '\d+'), 10)
+  describe 'each article' do
+    before :each do
+      @article = Article.generate!(:title => 'A Day in the Life', :content => 'yadda')
+      assigns[:articles] = [@article]
+      @title   = @article.title
+      @content = @article.content
+      template.stubs(:will_paginate)
+    end
+
+    it 'should include the title' do
+      do_render
+      response.should have_tag('li.article', Regexp.new(Regexp.escape(@title)))
+    end
+
+    it 'should include a link' do
+      do_render
+      response.should have_tag('li.article') do
+        with_tag('a[href=?]', article_path(@article))
+      end
     end
   end
-    
-  it 'should not error if the article is nil' do
-    assigns[:article] = nil
-    do_render
+
+  describe 'a slew of articles' do
+    before :each do
+      @articles = Array.new(2) { Article.generate!(:title => 'A Day in the Life', :content => 'yadda') }
+      assigns[:articles] = @articles
+      template.stubs(:will_paginate)
+    end
+
+    it 'should include a list of articles' do
+      do_render
+      response.should have_tag('ul.articles') do
+        @articles.each do |article|
+          response.should have_tag('li') do
+            with_tag('a[href=?]', article_path(article))
+          end
+        end
+      end
+    end
+  end    
+
+  describe 'articles pagination' do
+    before :each do
+      assigns[:articles] = @articles = [Article.generate!(:title => 'sunday, sunday, sunday')]
+    end
+
+    it 'should use will_paginate to setup links' do
+      template.expects(:will_paginate).with(@articles)
+      do_render
+    end
+
+    it 'should have ' do
+      @pagination_link_text = 'previous..next'
+      template.stubs(:will_paginate).returns(@pagination_link_text)
+      do_render
+      response.should have_text(Regexp.new(@pagination_link_text))
+    end
   end
 end
