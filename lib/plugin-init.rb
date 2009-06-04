4a04ffebb3462a191f0c626963bc3a2d647ecd80
Yossef Mendelssohn <ymendel@pobox.com>
2009-06-04 14:04:47 -0500

    Ensuring admin controller can be enhanced.

--- lib/plugin-init.rb
+++ lib/plugin-init.rb
@@ -1,2 +1,3 @@
 require File.expand_path(File.join(File.dirname(__FILE__), *%w[app models article]))
 require File.expand_path(File.join(File.dirname(__FILE__), *%w[app controllers articles_controller]))
+require File.expand_path(File.join(File.dirname(__FILE__), *%w[app controllers admin_controller]))
