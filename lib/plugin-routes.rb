d573db8cdba795372a0d049fa1b4f5ea84c9926e
Kevin R. Barnes <vinbarnes@gmail.com>
2009-06-05 12:37:06 -0500

    Update plugin-routes to match config/routes.
    Forgot to do this in 595bb161f68d9650eea91682039050ed071637e2 when I updated
    the article routing.

--- lib/plugin-routes.rb
+++ lib/plugin-routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :articles, :only => :index
+  map.resources :articles, :only => [:index, :show]
 
   map.namespace :admin do |admin|
     admin.resources :articles
