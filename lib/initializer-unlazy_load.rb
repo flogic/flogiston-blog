2b10fcef917a3c0ca05d0af1ce90e759fe116ea6
Yossef Mendelssohn <ymendel@pobox.com>
2009-12-14 16:40:47 -0600

    Making sure helpers are loaded correctly when used as plugin to other app.
    More loading/requiring bullshit hijinks.

--- lib/initializer-unlazy_load.rb
+++ lib/initializer-unlazy_load.rb
@@ -1,4 +1,5 @@
 models      = Dir[File.join(RAILS_ROOT, *%w[app models *])]
 controllers = Dir[File.join(RAILS_ROOT, *%w[app controllers *])]
+helpers     = Dir[File.join(RAILS_ROOT, *%w[app helpers *])]
 
 (models + controllers).select { |f|  File.file?(f) }.each { |f|  require f }
--- lib/plugin-init.rb
+++ lib/plugin-init.rb
@@ -1,3 +1,4 @@
 require File.expand_path(File.join(File.dirname(__FILE__), *%w[app models article]))
 require File.expand_path(File.join(File.dirname(__FILE__), *%w[app controllers articles_controller]))
 require File.expand_path(File.join(File.dirname(__FILE__), *%w[app controllers admin_controller]))
+require File.expand_path(File.join(File.dirname(__FILE__), *%w[app helpers articles_helper]))
